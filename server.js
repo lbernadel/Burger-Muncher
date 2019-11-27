ebe40902d5a930c0ac4ff39fd790b2ea22df77e7
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-11-27 16:25:55 -0500

    set up handlebars

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const express = require('express'),
 
 const PORT = process.env.PORT || 8080;
 
+
+//View engine
+app.engine("handlebars", exphbs({ defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+//Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+app.use(express.static("views"))
+
+//Routes
+app.get("/", (req, res) =>{
+    res.render("index")
+})
+
+//Tells server to listen
 app.listen(PORT, () => {
     console.log(`---> App is listening on http://localhost:${PORT}`)
 });
+
+
