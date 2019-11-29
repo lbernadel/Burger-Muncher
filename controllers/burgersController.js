01ab55e54e02adeb298bd803515bcea4962224d0
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-11-28 22:25:56 -0500

    api routes created

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,9 +1,41 @@
 const express = require('express'),
-    app = express();
+    app = express(),
+    db = require("../models/index");
 
 //Routes
 app.get("/", (req, res) =>{
-    res.render("index")
+    db.burgers.findAll()
+        .then(burger => {
+            res.render("index", {burgers: burger})
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
+
+app.post("/api/burgers", (req, res) => {
+    const {burger_name} = req.body
+
+    db.burgers.create({burger_name})
+        .then( newBurger => {
+            res.json(newBurger)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
+
+app.put("/api/burgers/:id", (req, res) => {
+    const {id} = req.params,
+        {isEaten} = req.body;
+    
+    db.update({ isEaten: isEaten }, { where: {id: id} })
+        .then(updateBurgers => {
+            res.json(updateBurgers)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
 })
 
 module.exports = app;
